2cb8ab92ed1aa5845f519847b8f50343975f7392
UDONGSALI <129227923+UDONGSALI@users.noreply.github.com>
2023-08-22 12:37:07 +0900

    2023/08/22 화요일
    카운터프로그램
    useEffect, usestate, useRef

--- han ib React/project1/src/component/Controller.js
+++ han ib React/project1/src/component/Controller.js
@@ -1,5 +1,5 @@
 const Controller = ({handleSetCount}) => {
-    return(
+    return (
         <div>
             <button onClick={() => handleSetCount(-1)}>-1</button>
             <button onClick={() => handleSetCount(-10)}>-10</button>
@@ -10,5 +10,4 @@ const Controller = ({handleSetCount}) => {
         </div>
     );
 };
-
 export default Controller;
